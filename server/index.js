e8a29bde052004122f58d1df28ea63b6bd4102eb
thenamannanda <naman.3727@gmail.com>
2023-01-29 22:59:20 +0530

    products data handling

--- server/index.js
+++ server/index.js
@@ -1,31 +1,31 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cors from 'cors';
-import dotenv from 'dotenv';
+import cors from "cors";
+import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import  clientRoutes from "./routes/client.js"
-import  generalRoutes from "./routes/general.js"
-import  salesRoutes from "./routes/sales.js"
-import  managementRoutes from "./routes/management.js"
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import salesRoutes from "./routes/sales.js";
+import managementRoutes from "./routes/management.js";
 
 //data imports
 import User from "./models/User.js";
-import {dataUser} from "./data/index.js"
-
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
 
 /* CONFIGURATION */
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy : "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-
 
 /* ROUTES */
 app.use("/client", clientRoutes);
@@ -33,18 +33,23 @@ app.use("/general", generalRoutes);
 app.use("management", managementRoutes);
 app.use("/sales", salesRoutes);
 
-
 /** MONGOOSE SETUP */
 
 const PORT = process.env.PORT || 9000;
-mongoose.set('strictQuery', false);
-;mongoose.connect(process.env.MONGO_URL, {
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
+  })
+  .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
     /**only add data one time */
-   // User.insertMany(dataUser);
-}).catch((error) => {
-    console.log("didn't connect")
-})
+    // User.insertMany(dataUser);
+    //Product.insertMany(dataProduct);
+
+    //ProductStat.insertMany(dataProductStat);
+  })
+  .catch((error) => {
+    console.log("didn't connect");
+  });
